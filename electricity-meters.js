71a2436830f624719e227c8c0496c831443258bb
Ncebakazi <hlomla.tapuko@gmail.com>
2022-01-19 12:58:50 +0200

    working on link to the street names

--- electricity-meters.js
+++ electricity-meters.js
@@ -8,28 +8,39 @@ module.exports = function(pool) {
 	}
 
 	// for a given street show all the meters and their balances
-	function streetMeters(streetId) {
-
+	async function streetMeters(streetId) {
+		const specificStreet =  await pool.query(`select * from street where id = $1`);
+		const showStreet = await pool.query(specificStreet, [streetId])
+		if(showStreet.rows.length > 0){
+			return showStreet.rows[0]
+		}
+		
 	}
 
 	// return all the appliances
-	function appliances() {
-
+	async function appliances() {
+		const allAppliance = await pool.query(`select * from appliance`)
+		return allAppliance.rows
 	}
 
 	// increase the meter balance for the meterId supplied
-	function topupElectricity(meterId, units) {
+	async function topupElectricity(meterId, units) {
 
 	}
 	
 	// return the data for a given balance
-	function meterData(meterId) {
+	async function meterData(meterId) {
 	
 	}
 
 	// decrease the meter balance for the meterId supplied
-	function useElectricity(meterId, units) {
+	async function useElectricity(meterId, units) {
 	
+	}
+
+	// show total balance for each street
+	async function totalStreetBalance(){
+
 	}
 
 	return {
@@ -38,7 +49,8 @@ module.exports = function(pool) {
 		appliances,
 		topupElectricity,
 		meterData,
-		useElectricity
+		useElectricity,
+		totalStreetBalance
 	}
 
 
--- index.js
+++ index.js
@@ -1,42 +1,45 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const ElectricityMeters = require('./electricity-meters');
 const pg = require('pg');
 const Pool = pg.Pool;
 
 const app = express();
-const PORT =  process.env.PORT || 3017;
 
-const ElectricityMeters = require('./electricity-meters');
+// var electricityMeters = ElectricityMeters(pool);
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/topups';
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/topups_db';
 
 const pool = new Pool({
     connectionString  
 });
 
-// enable the req.body object - to allow us to use HTML forms
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// enable the static folder...
-app.use(express.static('public'));
 
-// add more middleware to allow for templating support
+app.use(express.static('public'));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 const electricityMeters = ElectricityMeters(pool);
 
-app.get('/', function(req, res) {
+app.get('/', async function(req, res) {
+	
 	res.redirect('/streets');
 });
 
-app.get('/streets', async function(req, res) {
+app.get('/streets/meters/:id', async function(req, res) {
+	const streetId = req.params.id
+	const showStreet = await electricityMeters.streetMeters(streetId);
 	const streets = await electricityMeters.streets();
+	
 	console.log(streets);
+	console.log(showStreet)
 	res.render('streets', {
-		streets
+		streets,
+		showStreet
 	});
 });
 
@@ -69,6 +72,7 @@ app.post('/meter/use/:meter_id', async function(req, res) {
 
 });
 
+const PORT =  process.env.PORT || 3017;
 // start  the server and start listening for HTTP request on the PORT number specified...
 app.listen(PORT, function() {
 	console.log(`App started on port ${PORT}`)
